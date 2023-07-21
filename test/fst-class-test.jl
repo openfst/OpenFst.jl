91dfb6a6a8a76663b7d116d5244fe0b3107ec9ea
Lucas ONDEL YANG <lucas.ondel@cnrs.fr>
2023-07-21 16:28:31 +0200

    made the test more julianesque

--- test/fst-class-test.jl
+++ test/fst-class-test.jl
@@ -6,126 +6,66 @@ const tmpdir = "/tmp"
 
 f = F.read(testdir * "/f1const.fst")
 
-if !F.isfst(f)
-    error("test failed: read")
-end
-
-final = F.final(f, 1)
-if final != Inf
-    error("test failed: final")
-end
-
-arctype = F.arctype(f)
-if arctype != "standard"
-    error("test failed: arctype")
-end
-
-fsttype = F.fsttype(f)
-if fsttype != "const"
-    error("test failed: fsttype")
-end
-
-numarcs = F.numarcs(f, 1)
-if numarcs != 3
-    error("test failed: numarcs")
-end
-
-numieps = F.numinputepsilons(f, 1)
-if numieps != 1
-    error("test failed: numinputepsilons")
-end
-
-numoeps = F.numoutputepsilons(f, 1)
-if numoeps != 1
-    error("test failed: numoutputepsilons")
-end
-
-start = F.start(f)
-if start != 1
-    error("test failed: start")
-end
-
-weighttype = F.weighttype(f)
-if weighttype != "tropical"
-    error("test failed: weighttype")
-end
-
-res = F.write(f, tmpdir * "/f1.fst")
-if res == false
-    error("test failed: write")
-end
-
-tf = F.read(tmpdir * "/f1.fst")
-
-if !F.equal(f, tf)
-    error("test failed: write")
-end
+@test F.isfst(f)
+@test F.final(f, 1) == Inf
+@test F.arctype(f) == "standard"
+@test F.fsttype(f) == "const"
+@test F.numarcs(f, 1) == 3
+@test F.numinputepsilons(f, 1) == 1
+@test F.numoutputepsilons(f, 1) == 1
+@test F.start(f) == 1
+@test F.weighttype(f) == "tropical"
+@test F.write(f, tmpdir * "/f1.fst")
+@test F.equal(f, F.read(tmpdir * "/f1.fst"))
 
 # MutableFstClass
 goldenarcs = [F.Arc(1, 2, 1.0, 2), F.Arc(0, 0, 2.0, 2), F.Arc(5, 6, 3.0, 1)]
-
-mf = F.read(testdir * "/f1vector.fst");
+mf = F.read(testdir * "/f1vector.fst")
 
 F.setstart!(mf, 2)
-start = F.start(mf)
-if start != 2
-    error("test failed: setstart")
-end
+@test F.start(mf) == 2
+
 # restore
 F.setstart!(mf, 2)
 
 F.setfinal!(mf, 1, 1.0)
-final = F.final(mf, 1)
-if final != 1.0
-    error("test failed: setfinal")
-end
+@test F.final(mf, 1) == 1.0
+
 # restore
 F.setfinal!(mf, 1, Inf)
 
 F.deletearcs!(mf, 1);
 F.reservearcs(mf, 1, 3);
-if F.numarcs(mf, 1) != 0
-    error("test failed: deletearcs")
-end
+@test F.numarcs(mf, 1) == 0
 
 for a in goldenarcs
     F.addarc!(mf, 1, a)
 end
-
 F.deletearcs!(mf, 1)
+@test F.numarcs(mf, 1) == 0
 
-if F.numarcs(mf, 1) != 0
-    error("test failed: deletearcs")
-end
-
-if F.numstates(mf) != 2
-    error("test failed: numstates")
-end
+@test F.numstates(mf) == 2
 
 F.reservestates(mf, 5);
 F.addstate!(mf)
+@test F.numstates(mf) == 3
 
-if F.numstates(mf) != 3
-    error("test failed: numstates")
-end
-
-F.deletestates!(mf);
+F.deletestates!(mf)
 
 # VectorFstClass
 
 svecf = F.StdVectorFst()
-if !F.isfst(svecf) || F.fsttype(svecf) != "vector" || 
-    F.arctype(svecf) != "standard" || typeof(svecf) != F.VectorFst{F.TropicalWeight}
-    error("test failed: StdVectorFst construction")
-end
+@test (F.isfst(svecf) &&
+       F.fsttype(svecf) == "vector" &&
+       F.arctype(svecf) == "standard" &&
+       typeof(svecf) == F.VectorFst{F.TropicalWeight})
 
 lvecf = F.VectorFst{F.LogWeight}()
-if !F.isfst(lvecf) || F.fsttype(lvecf) != "vector" || 
-    F.arctype(lvecf) != "log" || typeof(lvecf) != F.VectorFst{F.LogWeight}
-    error("test failed: Log VectorFst construction")
-end
+@test (F.isfst(lvecf) &&
+       F.fsttype(lvecf) == "vector" &&
+       F.arctype(lvecf) == "log" &&
+       typeof(lvecf) == F.VectorFst{F.LogWeight})
 
 copyf = F.VectorFst(f)
-if !F.equal(copyf, f)
-    error("test failed: copy")
-end
+@test F.equal(copyf, f)
+
--- test/fst-ops-test.jl
+++ test/fst-ops-test.jl
@@ -3,76 +3,52 @@ const testdir = "./testdata"
 const tmpdir = "/tmp"
 
 f1 = F.read(testdir * "/f1const.fst")
-if !F.isfst(f1)
-    error("test failed: read")
-end
+@test F.isfst(f1)
 
 f2 = F.read(testdir * "/f2.fst")
-if !F.isfst(f2)
-    error("test failed: read")
-end
+@test F.isfst(f2)
 
 a1 = F.read(testdir * "/a1.fst")
-if !F.isfst(a1)
-    error("test failed: read")
-end
+@test F.isfst(a1)
 
 a2 = F.read(testdir * "/a2.fst")
-if !F.isfst(a2)
-    error("test failed: read")
-end
+@test F.isfst(a2)
 
 asrtf = F.VectorFst(f1)
 F.arcsort!(asrtf, F.ilabel)
 goldenasrtf = F.read(testdir * "/f1arcsort.fst")
-if !F.isfst(asrtf) || !F.equal(asrtf, goldenasrtf)
-    error("test failed: arcsort")
-end
+@test F.isfst(asrtf) && F.equal(asrtf, goldenasrtf)
 
 cf = F.compose(f1, f2)
 goldencf = F.read(testdir * "/f12compose.fst")
-if !F.isfst(cf) || !F.equal(cf, goldencf)
-    error("test failed: compose")
-end
+@test F.isfst(cf) && F.equal(cf, goldencf)
 
 trmf = F.VectorFst(a1)
 F.connect!(trmf)
-if !F.isfst(trmf) || !F.equal(a1, trmf)
-    error("test failed: connect")
-end
+@test F.isfst(trmf) && F.equal(a1, trmf)
 
 epsnf = F.epsnormalize(f1, F.eps_norm_input)
 goldenepsnf = F.read(testdir * "/f1epsnorm.fst")
-if !F.isfst(epsnf) || !F.equal(epsnf, goldenepsnf)
-    error("test failed: epsnormalize")
-end
+@test F.isfst(epsnf) && F.equal(epsnf, goldenepsnf)
 
 inf = F.intersect(a1, a2)
 goldeninf = F.read(testdir * "/a12intersect.fst")
-if !F.isfst(inf) || !F.equal(inf, goldeninf)
-    error("test failed: intersect")
-end
+@test F.isfst(inf) && F.equal(inf, goldeninf)
 
 conf = F.VectorFst(a1)
 F.concat!(conf, a2)
 goldenconf = F.read(testdir * "/a12concat.fst")
-if !F.isfst(conf) || !F.equal(conf, goldenconf)
-    error("test failed: concat")
-end
+@test F.isfst(conf) && F.equal(conf, goldenconf)
 
 clsf = F.VectorFst(a1)
 F.closure!(clsf)
 goldenclsf = F.read(testdir * "/a1closure.fst")
-if !F.isfst(clsf) || !F.equal(clsf, goldenclsf)
-    error("test failed: closure")
-end
+@test F.isfst(clsf) && F.equal(clsf, goldenclsf)
 
 unf1 = F.VectorFst(a1)
 F.union!(unf1, a2)
 goldenunf = F.read(testdir * "/a12union.fst")
-if !F.isfst(unf1) || !F.equal(unf1, goldenunf)
-    error("test failed: union")
-end
+@test F.isfst(unf1) && F.equal(unf1, goldenunf)
 
 unf2 = F.VectorFst(a2)
 F.union!(unf2, a1)
@@ -80,86 +56,59 @@ F.rmepsilon!(unf1)
 F.rmepsilon!(unf2)
 dunf1 = F.determinize(unf1)
 dunf2 = F.determinize(unf2)
-if !F.equivalent(dunf1, dunf2)
-    error("test failed: equivalent")
-end
+@test F.equivalent(dunf1, dunf2)
 
 invf = F.VectorFst(f1)
 goldeninvf = F.read(testdir * "/f1invert.fst")
 F.invert!(invf)
-if !F.isfst(invf) || !F.equal(invf, goldeninvf)
-    error("test failed: invert")
-end
+@test F.isfst(invf) && F.equal(invf, goldeninvf)
 
 projf = F.VectorFst(f1)
 F.project!(projf, F.output)
 goldenprojf = F.read(testdir * "/f1project.fst")
-if !F.isfst(projf) || !F.equal(projf, goldenprojf)
-    error("test failed: project")
-end
+@test F.isfst(projf) && F.equal(projf, goldenprojf)
 
 rmf = F.VectorFst(f1)
 goldenrmf = F.read(testdir * "/f1rmepsilon.fst")
 F.rmepsilon!(rmf)
-if !F.isfst(rmf) || !F.equal(rmf, goldenrmf)
-    error("test failed: rmepsilon")
-end
+@test F.isfst(rmf) && F.equal(rmf, goldenrmf)
 
 detf = F.determinize(rmf)
 goldendetf = F.read(testdir * "/f1determinize.fst")
-if !F.isfst(detf) || !F.equal(detf, goldendetf)
-    error("test failed: determinize")
-end
+@test F.isfst(detf) && F.equal(detf, goldendetf)
 
 disf = F.disambiguate(rmf)
 goldendisf = F.read(testdir * "/f1disambiguate.fst")
-if !F.isfst(disf) || !F.equal(disf, goldendisf)
-    error("test failed: disambiguate")
-end
+@test F.isfst(disf) && F.equal(disf, goldendisf)
 
 minf = F.VectorFst(detf)
 F.minimize!(minf)
 goldenminf = F.read(testdir * "/f1minimize.fst")
-if !F.isfst(minf) || !F.equal(minf, goldenminf)
-    error("test failed: minimize")
-end
+@test F.isfst(minf) && F.equal(minf, goldenminf)
 
 prf = F.VectorFst(f)
 F.prune!(prf, 0.5)
 goldenpruf = F.read(testdir * "/f1prune.fst")
-if !F.isfst(prf) || !F.equal(prf, goldenpruf)
-    error("test failed: prune")
-end
+@test F.isfst(prf) && F.equal(prf, goldenpruf)
 
 pshf = F.VectorFst(f)
 F.push!(pshf, F.reweight_to_initial)
 goldenpshf = F.read(testdir * "/f1push.fst")
-if !F.isfst(pshf) || !F.equal(pshf, goldenpshf)
-    error("test failed: push")
-end
+@test F.isfst(pshf) && F.equal(pshf, goldenpshf)
 
 revf = F.reverse(f1)
 goldenrevf = F.read(testdir * "/f1reverse.fst")
-if !F.isfst(revf) || !F.equal(revf, goldenrevf)
-    error("test failed: reverse")
-end
+@test F.isfst(revf) && F.equal(revf, goldenrevf)
 
 topf = F.VectorFst(a2)
 F.topsort(topf)
-if !F.isfst(topf) || !F.isomorphic(a2, topf)
-    error("test failed: topsort")
-end
+@test F.isfst(topf) && F.isomorphic(a2, topf)
 
 distance = F.shortestdistance(f1)
 print(distance[2])
-if distance[2] != 1.0
-    error("test failed: shortestdistance")
-end
+@test distance[2] == 1.0
 
 shrtf = F.shortestpath(f1)
 goldenshrtf = F.read(testdir * "/f1shortest.fst")
-if !F.isfst(shrtf) || !F.equal(shrtf, goldenshrtf)
-    error("test failed: shortestpath")
-end
-
+@test F.isfst(shrtf) && F.equal(shrtf, goldenshrtf)
 
